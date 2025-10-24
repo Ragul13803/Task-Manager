e2392d9a9133c4b159b50912234bb7bc12416605
Ragul13803 <rglsrsh@gmail.com>
2025-10-24 13:46:38 +0530

    Set up Redux store and sagas for task management

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -5,7 +5,7 @@ import { addTaskRequest } from '../redux/taskSlice';
 
 const TaskForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); 
   const loading = useSelector(state => state.tasks.loading);
 
   const onSubmit = (data) => {
